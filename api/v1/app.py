bcd4b5b3f606530f47410b6894df47bcfe09338c
M-sharkawy <mostafasharks021@gmail.com>
2025-02-15 22:58:35 +0200

    edit error messagefor 404 edits

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 AirBnB_clone_v3 API constructor
 '''
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
