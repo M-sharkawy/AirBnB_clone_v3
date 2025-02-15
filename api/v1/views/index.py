74d4360791d68ea52c7af9b74955fac4e4786aa9
M-sharkawy <mostafasharks021@gmail.com>
2025-02-15 22:24:31 +0200

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python3
 '''
-
+app views fo airbnb
 '''
 from flask import jsonify
+from models import storage
 from api.v1.views import app_views
+
 
 @app_views.route('/status', methods=["GET"])
 def get_data():
-    """ returns status """
+    '''returns status'''
     status = {"status": "OK"}
     return jsonify(status)
+
+
+@app_views.route('/stats', methods=["GET"])
+def count_():
+    '''retrieves the number of each objects by type'''
+    total = {}
+    classes = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+        }
+
+    for cls in classes:
+        count = storage.count(cls)
+        total[classes.get(cls)] = count
+
+    return jsonify(total)
