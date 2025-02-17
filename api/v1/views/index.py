7ab598844ff9f0038f7746283a3d52dfcfd53f75
root <mostafasharks021@gmail.com>
2025-02-17 13:39:03 -0800

    checker edits

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,7 +10,9 @@ from api.v1.views import app_views
 @app_views.route('/status', methods=["GET"])
 def get_data():
     '''returns status'''
-    status = {"status": "OK"}
+    status = {
+            "status": "OK"
+            }
     return jsonify(status)
 
 
