546a31bf6ad73ebeb59d71bc1006a1159601d814
M-sharkawy <mostafasharks021@gmail.com>
2025-02-17 18:32:34 +0200

    checker edits

---
+++ api/v1/app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+AirBnB_clone_v3 API constructor
+'''
+from flask import Flask, jsonify
+from api.v1.views import app_views
+from models import storage
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_app(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def notfounderror(error):
+    '''handle error message'''
+    status = {"error": "Not found"}
+    return jsonify(status), 404
+
+
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
---
+++ api/v1/views/users.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""users Routes file"""
+
+from models import storage
+from models.user import User
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+
+
+@app_views.route("/users", methods=["GET"],
+                 strict_slashes=False)
+def get_list():
+    '''
+    return list for all User objects
+    '''
+    usr_list = []
+    users = storage.all(User)
+    for user in users.values():
+        usr_list.append(user.to_dict())
+    return jsonify(usr_list)
+
+
+@app_views.route("/places/<user_id>", methods=["GET"],
+                 strict_slashes=False)
+def get_usr_id(user_id):
+    '''
+    return user object by id
+    '''
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route("users/<user_id>", methods=["DELETE"],
+                 strict_slashes=False)
+def delete_obj(user_id):
+    '''
+    delete user obj according to id
+    '''
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route("/users", methods=["POST"])
+def update_userlist():
+    '''
+    Create user list
+    '''
+    data = request.get_json()
+
+    if not data:
+        abort(400, description="Not a JSON")
+    if "email" not in data:
+        abort(400, description="Missing email")
+    if "password" not in data:
+        abort(400, description="Missing password")
+
+    new_user = User(**data)
+    storage.new(new_user)
+    storage.save()
+
+    return jsonify(new_user.to_dict()), 201
+
+
+@app_views.route("/users/<user_id>", methods=["PUT"],
+                 strict_slashes=False)
+def put_city(user_id):
+    """Updates a User object"""
+    city = storage.get(User, user_id)
+    if not city:
+        abort(404)
+
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+
+    data.pop("id", None)
+    data.pop("state_id", None)
+    data.pop("created_at", None)
+    data.pop("updated_at", None)
+
+    for key, value in data.items():
+        setattr(city, key, value)
+
+    storage.save()
+    return (jsonify(city.to_dict()), 200)
